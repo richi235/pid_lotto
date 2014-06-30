66830955df9faad95c8bff391dde276edda25c80
Coop User <coop@parsifal.(none)>
2014-06-30 18:53:14 +0200

    developement progress

--- generate_pid_lottotips.sh
+++ generate_pid_lottotips.sh
@@ -27,25 +27,58 @@ hashed_pids=`echo "ibase=16 ; $hashed_pids" | bc`
 #      modulo operator and the +1
 #      the 10# is necessary to avoid bash thinks that numbers like 08 are octal 
 #          and therefore throws an error because of, "octal number to big"
-lotto_tip_1=$(( (10#${hashed_pids:0:2} % 49) + 1 ))
-lotto_tip_2=$(( (10#${hashed_pids:2:2} % 49) + 1 ))
-lotto_tip_3=$(( (10#${hashed_pids:4:2} % 49) + 1 ))
-lotto_tip_4=$(( (10#${hashed_pids:6:2} % 49) + 1 ))
-lotto_tip_5=$(( (10#${hashed_pids:8:2} % 49) + 1 ))
-lotto_tip_6=$(( (10#${hashed_pids:10:2} % 49) + 1 ))
+ball_to_pick_1=$(( (10#${hashed_pids:0:2} % 49) + 1 ))
+ball_to_pick_2=$(( (10#${hashed_pids:2:2} % 48) + 1 ))
+ball_to_pick_3=$(( (10#${hashed_pids:4:2} % 47) + 1 ))
+ball_to_pick_4=$(( (10#${hashed_pids:6:2} % 46) + 1 ))
+ball_to_pick_5=$(( (10#${hashed_pids:8:2} % 45) + 1 ))
+ball_to_pick_6=$(( (10#${hashed_pids:10:2} % 44) + 1 ))
 
 # superzahl in german lotteria
 lotto_supernumber=${hashed_pids:12:1}
 
 # atm it's possible that 2 tip numbers are the same, the following code avoids this
+lotto_balls=()
+
+for ((i=1 ; i<50 ; i++ )) ; do
+    lotto_balls[$i]=$i 
+done    
+
+# pick lotto ball 1:
+lotto_ball_1=${lotto_balls[$ball_to_pick_1]}
+lotto_balls[$ball_to_pick_1]=${lotto_balls[49]}
+unset lotto_balls[49]
+
+# pick lotto ball 2:
+lotto_ball_2=${lotto_balls[$ball_to_pick_2]}
+lotto_balls[$ball_to_pick_2]=${lotto_balls[48]}
+unset lotto_balls[48]
+
+# pick lotto ball 3:
+lotto_ball_3=${lotto_balls[$ball_to_pick_3]}
+lotto_balls[$ball_to_pick_3]=${lotto_balls[47]}
+unset lotto_balls[47]
+
+# pick lotto ball 4:
+lotto_ball_4=${lotto_balls[$ball_to_pick_4]}
+lotto_balls[$ball_to_pick_4]=${lotto_balls[46]}
+unset lotto_balls[46]
+
+# pick lotto ball 5:
+lotto_ball_5=${lotto_balls[$ball_to_pick_5]}
+lotto_balls[$ball_to_pick_5]=${lotto_balls[45]}
+unset lotto_balls[45]
+
+# pick lotto ball 6:
+lotto_ball_6=${lotto_balls[$ball_to_pick_6]}
+lotto_balls[$ball_to_pick_6]=${lotto_balls[44]}
+unset lotto_balls[44]
+
+# generate one string of sorted numbers from the 6 integers
+lotto_tip_string=`echo -e "$lotto_ball_1\n$lotto_ball_2\n$lotto_ball_3\n$lotto_ball_4\n$lotto_ball_5\n$lotto_ball_6" | sort -n`
+lotto_tip_string=`echo $lotto_tip_string | sed 's:\n: :'`
 
 
 
-
-echo $lotto_tip_1
-echo $lotto_tip_2
-echo $lotto_tip_3
-echo $lotto_tip_4
-echo $lotto_tip_5
-echo $lotto_tip_6
-echo $lotto_supernumber
+# echo $lotto_tip_string
+# echo "Supernumber:"$lotto_supernumber
--- main_business_logic.sh
+++ main_business_logic.sh
@@ -2,24 +2,47 @@
 
 # show a nice terminal UI to the User and let him select what to do next
 whiptail --title "Enterprise PID Lotto" \
- --menu "\nWelcome to the Enterprise Business PID Lotto Suite. \n\nChoose what to do: \n\n" 30 78 16 \
- "1."         "    Alle deutschen Lotto-Ergebnisse aus dem Weltnetz herabladen." \
- "2."         "    Lotto Tipp aus PIDs erschaffen und Glueck versuchen" 2> /tmp/whiptail_choice 
+   --menu "\nWelcome to the Enterprise Business PID Lotto Suite. \n\nChoose what to do: \n\n" 30 78 16 \
+    "1."         "    Alle deutschen Lotto-Ergebnisse aus dem Weltnetz herabladen." \
+    "2."         "    Lotto Tipp aus PIDs erschaffen und Glueck versuchen" 2> /tmp/whiptail_choice 
 
-answer=`cat /tmp/whiptail_choice`
+user_choice=`cat /tmp/whiptail_choice`
 
-if [ "$answer" = "1."  ]; then  # get lotto results from internet
+
+# Do Things according to user selection
+if [ "$user_choice" = "1."  ]; then  # get lotto results from internet
     source ./get_lottozahlen.sh lottozahlen
     source ./adjust_output.sh lottozahlen
 
+elif [ "$user_choice" = "2."  ]; then  # check for match
+    source ./generate_pid_lottotips.sh
+    # lotto_tip_string="3 12 18 30 32 49"  # an actual winning tip string , without Supernumber, here for testing 
+     #lotto_tip_string="1 5 16 19 24 48" # an actual winning tip string , with Supernumber, here for testing 
+     #lotto_supernumber="5"
 
-elif [ "$answer" = "2."  ]; then  # check for match
-    source ./generate_pid_lottotips_unique.sh
-
-    # lotto_tip_string="3 12 18 30 32 49"  # an actual winning tip string , here for testing
-
-    # look for Hits without supernumber
+    # check for Hits without supernumber
     grep -E -e "^.{11}$lotto_tip_string" lottozahlen_ohne_SZ
-    # look for Hits with supernumber
+
+    # Display congratulation message if grep matches something
+    if (($?==0)) ; then 
+        whiptail --title "Gewonnen !1!!!1!!! " \
+                 --msgbox "Herzlichen Glueckwunsch sie haben mit den Lottozahlen \n\n    $lotto_tip_string \n\ngewonnen. \n\nUeberabzaehlbar unendlicher Reichtum erwartet dich." 30 78
+        exit
+    fi  # check for matches with supernumber only if no match happened:
+        
+       
+
+
+    # check for Hits with supernumber
     grep -E -e "^.{11}$lotto_tip_string $lotto_supernumber" lottozahlen_mit_SZ
+    if (($?==0)) ; then 
+        whiptail --title "Gewonnen !1!!!1!!! " \
+                 --msgbox "Herzlichen Glueckwunsch Sie haben mit den Lottozahlen \n\n    $lotto_tip_string und Superzahl: $lotto_supernumber  \n\ngewonnen. \n\nUeberabzaehlbar unendlicher Reichtum erwartet dich." 30 78
+        exit
+    elif (($? == 1)) ; then
+        whiptail --title "Verloren !1!!!1!!!" \
+                 --msgbox "Sie haben eine Niete gezogen. Ihre Zahlen waren \n\n    $lotto_tip_string mit Superzahl: $lotto_supernumber \n\n." 30 78
+        exit
+    fi
+
 fi
