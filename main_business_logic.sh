49689048bb29e315183735f105d545f88f32b1d8
Coop User <coop@parsifal.(none)>
2014-06-30 19:28:26 +0200

    minor changes

--- main_business_logic.sh
+++ main_business_logic.sh
@@ -16,17 +16,21 @@ if [ "$user_choice" = "1."  ]; then  # get lotto results from internet
 
 elif [ "$user_choice" = "2."  ]; then  # check for match
     source ./generate_pid_lottotips.sh
-    # lotto_tip_string="3 12 18 30 32 49"  # an actual winning tip string , without Supernumber, here for testing 
-     #lotto_tip_string="1 5 16 19 24 48" # an actual winning tip string , with Supernumber, here for testing 
-     #lotto_supernumber="5"
+     lotto_tip_string="3 12 18 30 32 49"  # an actual winning tip string , without Supernumber, here for testing 
+     lotto_tip_string="1 5 16 19 24 48" # an actual winning tip string , with Supernumber, here for testing 
+     lotto_supernumber="5"
 
     # check for Hits without supernumber
-    grep -E -e "^.{11}$lotto_tip_string" lottozahlen_ohne_SZ
+    grep -E -e "^.{11}$lotto_tip_string" lottozahlen_ohne_SZ > /tmp/winning_time 
+    grep_return_value=$?
+    winning_time=`cat /tmp/winning_time`
+    winning_time=${winning_time:0:10}
+    #echo $winning_time
 
     # Display congratulation message if grep matches something
-    if (($?==0)) ; then 
+    if (($grep_return_value==0)) ; then 
         whiptail --title "Gewonnen !1!!!1!!! " \
-                 --msgbox "Herzlichen Glueckwunsch sie haben mit den Lottozahlen \n\n    $lotto_tip_string \n\ngewonnen. \n\nUeberabzaehlbar unendlicher Reichtum erwartet dich." 30 78
+                 --msgbox "Herzlichen Glueckwunsch Sie haben mit den Lottozahlen \n\n    $lotto_tip_string \n\n am $winning_time gewonnen. \n\nAbzaehlbar unendlicher Reichtum erwartet Sie!" 30 78
         exit
     fi  # check for matches with supernumber only if no match happened:
         
@@ -35,9 +39,14 @@ elif [ "$user_choice" = "2."  ]; then  # check for match
 
     # check for Hits with supernumber
     grep -E -e "^.{11}$lotto_tip_string $lotto_supernumber" lottozahlen_mit_SZ
+    grep_return_value=$?
+    winning_time=`cat /tmp/winning_time`
+    winning_time=${winning_time:0:10}
+    #echo $winning_time
+
     if (($?==0)) ; then 
         whiptail --title "Gewonnen !1!!!1!!! " \
-                 --msgbox "Herzlichen Glueckwunsch Sie haben mit den Lottozahlen \n\n    $lotto_tip_string und Superzahl: $lotto_supernumber  \n\ngewonnen. \n\nUeberabzaehlbar unendlicher Reichtum erwartet dich." 30 78
+                 --msgbox "Herzlichen Glueckwunsch Sie haben mit den Lottozahlen \n\n    $lotto_tip_string und Superzahl: $lotto_supernumber  \n\n am $winning_time gewonnen. \n\nUeberabzaehlbar unendlicher Reichtum erwartet Sie!" 30 78
         exit
     elif (($? == 1)) ; then
         whiptail --title "Verloren !1!!!1!!!" \
